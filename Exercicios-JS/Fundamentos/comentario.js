b173551d14d0c1b0180a3f3a3ac7345285a39f8f
Lucas <lucasreinaldosilva@outlook.com>
2022-05-26 10:02:31 -0300

    Fundamentos

---
+++ Exercicios-JS/Fundamentos/comentario.js
@@ -0,0 +1,13 @@
+// comentário de linha
+console.log('Linha 1')
+
+/*
+comentário de
+multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+* comentadio de
+* multiplas linhas
+*/
---
+++ Exercicios-JS/Fundamentos/variaveiseConstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4 // forma mais moderna de criar variaveis
+
+var a = 30 // var pode ser redeclarado
+b = 40 // let não pode ser redeclarado 
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5
+
+console.log(c)
